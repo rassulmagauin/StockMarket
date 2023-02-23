46b4736c0794e60419ae28fca933db86294a9b5e
Rassul Magauin <rassulmagauin@gmail.com>
2023-02-23 11:37:05 +0600

    secret key hiding

--- StockMarket/settings.py
+++ StockMarket/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8*nej_neoz$r(fj=&fbzrotq-4tc@jto8h2p7vrlo=t^lk3b$x'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
